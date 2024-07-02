6be71d12525fc8f9f3bd029f9aca05b253fc090c
Matthew Beck <mtb2tgk@virginia.edu>
2024-07-02 18:49:26 -0400

    Update HW

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name = 'books',
+    name = 'booklover',
     version = '0.1.0',
     author = 'Matthew Beck',
     author_email = 'mtb2tgk@virginia.edu',
-    packages = ['books'],
+    packages = ['booklover'],
     url = 'https://github.com/mattbeck1/DS5100-mtb2tgk-HW09/blob/main/hw09.ipynb',
     license = 'LICENSE.txt',
     description = 'An awesome package with the booklover module',
